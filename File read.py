5d7ad92913956ced19b5f985cf16d7d1b63ec83c
JoeTheBeginner <45625062+JoeTheBeginner@users.noreply.github.com>
2018-12-05 11:27:25 +0100

    Add files via upload

---
+++ File read.py
@@ -0,0 +1,25 @@
+class randw :
+ def __init__(self, read_from , write_to):   
+   self.filename1 = read_from
+   self.filename2 = write_to
+ def reader(self):
+   x = open(self.filename1, "r")
+   global liste
+   liste = []
+   for line in x:
+      line = line.strip()
+      liste.append(line)
+   return liste
+ def writer(self):
+   y = open(self.filename2,'w')
+  
+   for n in liste :
+       y.write( n + ' \n')
+   y = open(self.filename2,'r')
+   for line in y:
+       print(line.strip())
+
+readwrite = randw('lesen.txt',"write.txt")
+
+readwrite.reader()
+readwrite.writer()
